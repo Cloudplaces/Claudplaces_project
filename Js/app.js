f3d965baec74981ad385dd291089377a1e77eefe
fmmricardo <ricardofilipe5@sapo.pt>
2018-03-21 09:10:28 +0000

    File with navbar

---
+++ Js/app.js
@@ -0,0 +1,5 @@
+function next() {
+    // document.getElementById("demo").innerHTML = "Hello World";
+    var secondCameraEl = document.querySelector('#second-camera');
+    secondCameraEl.setAttribute('camera', 'active', true);
+}
